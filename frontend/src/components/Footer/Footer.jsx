e321b728aac7fd54d2c0ebcd80125a58c7377349
Deepak Kumar <deep7654k@gmail.com>
2025-07-10 19:55:20 +0530

    footer Done For LOgin

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='flex justify-center items-center bg-amber-400 p-3'>
+      Footer am
+    </div>
+  )
+}
